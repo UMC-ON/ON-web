3fe22b63a0b4b206ea3e6f91999ac71916b0dde9
HwangJaemin49 <ch06159@gmail.com>
2024-08-21 07:23:50 +0900

    JM/87-CHORE 판매 물품 목록 필터링 기능 및 검색 기능 추가

--- ON-FRONTEND/src/components/DDayCalendar.jsx
+++ ON-FRONTEND/src/components/DDayCalendar.jsx
@@ -1,4 +1,3 @@
-// DDayCalendarComponent.jsx
 import React from 'react';
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
@@ -45,69 +44,6 @@ const DDayCalendarComponent = ({ selectedDate, handleDateChange, setCalendarOpen
 export default DDayCalendarComponent;
 
 
-
-// import React from 'react';
-// import styled from 'styled-components';
-// import DatePicker from 'react-datepicker';
-// import moment from 'moment';
-// import ko from 'date-fns/locale/ko';
-// import axios from 'axios';
-
-// const DDayCalendarComponent = ({ selectedDate, handleDateChange, setCalendarOpen, datePickerRef }) => {
-
-//   const handleDateSelection = async (date) => {
-//     // 날짜가 변경될 때마다 부모 컴포넌트에 변경 사항을 전달
-//     handleDateChange(date);
-
-//     // 서버에 POST 요청 보내기
-//     try {
-//       const response = await axios.post('/api/v1/diary/startdate', {
-//         startDate: moment(date).format('YYYY-MM-DD'),
-//       });
-
-//       console.log('서버 응답:', response.data);
-//     } catch (error) {
-//       console.error('날짜 저장 중 오류 발생:', error);
-//     }
-//   };
-
-//   return (
-//     <DDayCalendar>
-//       <DatePickerWrapper>
-//         <DatePicker
-//           showPopperArrow={false}
-//           locale={ko}
-//           className='inputDate'
-//           placeholderText={'날짜 설정'}
-//           ref={datePickerRef}
-//           selected={selectedDate}
-//           onChange={handleDateSelection} // 날짜 선택 시 처리
-//           dateFormat="yyyy-MM-dd"
-//           renderCustomHeader={({
-//             date,
-//             decreaseMonth,
-//             increaseMonth,
-//           }) => (
-//             <HeaderContainer>
-//               <Arrow onClick={decreaseMonth}>{'<'}</Arrow>
-//               <HeaderDate>{moment(date).format('YYYY.MM')}</HeaderDate>
-//               <Arrow onClick={increaseMonth}>{'>'}</Arrow>
-//             </HeaderContainer>
-//           )}
-//           popperModifiers={{
-//             offset: {
-//               enabled: true,
-//               offset: '0px, 10px',
-//             },
-//           }}
-//           onClickOutside={() => setCalendarOpen(false)}
-//         />
-//       </DatePickerWrapper>
-//     </DDayCalendar>
-//   );
-// };
-
-// export default DDayCalendarComponent;
 
 
 const DDayCalendar = styled.div`
