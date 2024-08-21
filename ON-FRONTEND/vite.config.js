33a1aafb29c2711cba3791c3871c807e5a176fa1
challonsy <musain2019@naver.com>
2024-08-22 08:04:41 +0900

    CY

--- ON-FRONTEND/vite.config.js
+++ ON-FRONTEND/vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+<<<<<<< Updated upstream
   server: {
     port: 443,
   },
   plugins: [react(), vercel()],
+=======
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(
+        'C:/Users/user/Documents/GitHub/ON-web/ON-FRONTEND',
+        './src',
+      ),
+    },
+  },
+>>>>>>> Stashed changes
 });
