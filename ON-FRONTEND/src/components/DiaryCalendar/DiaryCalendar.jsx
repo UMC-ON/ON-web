b3e613f66703f6111f21058bc3ae84eb2b815b9c
HwangJaemin49 <ch06159@gmail.com>
2024-08-22 03:19:28 +0900

    수정

--- ON-FRONTEND/src/components/DiaryCalendar/DiaryCalendar.jsx
+++ ON-FRONTEND/src/components/DiaryCalendar/DiaryCalendar.jsx
@@ -5,17 +5,16 @@ import * as s from './DiaryCalendarStyled.jsx';
 
 import diaryCheck from "../../assets/images/diaryCheck.svg";
 
-export default function DiaryCalendar({ diaries }) {
+export default function DiaryCalendar({ diaries, dateList }) {
   const [value, onChange] = useState(new Date());
 
   // 일기 작성 날짜 리스트 (백엔드에서 받아온 일기 날짜 리스트)
-  const dayList = diaries.dateList || [];
 
   // 각 날짜 타일에 컨텐츠 추가
   const tileContent = ({ date, view }) => {
     if (view === 'month') {
       // 날짜가 dayList에 포함된 경우 하트를 출력, 그렇지 않으면 기본 체크박스를 출력
-      if (dayList.includes(moment(date).format('YYYY-MM-DD'))) {
+      if (dateList.includes(moment(date).format('YYYY-MM-DD'))) {
         return <s.FullCheckbox><s.DiaryCheck src = {diaryCheck} /></s.FullCheckbox>;
       } else {
         return <s.EmptyCheckbox />;
