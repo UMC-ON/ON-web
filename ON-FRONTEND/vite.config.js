32abc780c6355572d539845c31c364b4a0785480
Seojung <sjhan0814@gmail.com>
2024-08-22 03:29:30 +0900

    react

--- ON-FRONTEND/vite.config.js
+++ ON-FRONTEND/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
     port: process.env.PORT,
   },
-  plugins: [vercel()],
+  plugins: [react(), vercel()],
 });
